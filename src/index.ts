43a203f5d16532269a8e796a8164395468a9ae3c
Farsana kt <ffarsanakt@gmail.com>
2025-07-30 22:20:28 +0530

    updated index.ts

--- src/index.ts
+++ src/index.ts
@@ -14,14 +14,43 @@ const PORT=process.env.PORT
 
 
 connectMonogoDb()
+
+// app.use(
+//   cors({
+//     origin: ['http://localhost:8080', 'https://ibookingvenue.com'],
+//     credentials: true,
+//   })
+// );
+
+const allowedOrigins = [
+    process.env.FRONTEND_URL as string,
+    process.env.ADMIN_URL as string,
+    'http://localhost:8080',
+    'https://ibookingvenue.com',
+    
+];
+
+app.use(
+    cors({
+        origin: function (origin, callback) {
+            if (!origin || allowedOrigins.includes(origin)) {
+                console.log(origin, "origin when cors is used");
+                callback(null, origin);
+            } else {
+                console.log(origin, allowedOrigins, "origin when cors is not used");
+                callback(new Error("Not allowed by CORS"));
+            }
+        },
+        credentials: true,
+        methods: ["GET", "POST", "PATCH", "DELETE", "PUT"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+    })
+);
+
+
+
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: ['http://localhost:8080', 'https://ibookingvenue.com'],
-    credentials: true,
-  })
-);
 
 app.use('/',auditoriumAuth_route)
 app.use('/',userAuth_route)
